1baaf8195f345a8995b2907e40342f47d201d016
samanthjain <samanthjainsb@gmail.com>
2024-10-30 00:57:54 +0530

    changes

--- codetest/settings.py
+++ codetest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5qu$&51743212w**+4c$kyhk(tk1gme(2z2^s$k0=!b52e+fmr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'editor',
     'rest_framework',
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "codetest.urls"
